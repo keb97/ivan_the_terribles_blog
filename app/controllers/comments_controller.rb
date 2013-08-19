407ef5126626ba5507d0a8fc99a1212d836a7113
Kathryn <keb97@cornell.edu>
2013-08-19 16:22:01 -0700

    adds some eager loading

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.joins(:replies).page(params[:page]).per_page(5)
 
     respond_to do |format|
       format.html # index.html.erb
--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,7 +2,7 @@ class RepliesController < ApplicationController
   # GET /replies
   # GET /replies.json
   def index
-    @replies = Reply.all
+    @replies = Reply.page(params[:page]).per_page(5)
 
     respond_to do |format|
       format.html # index.html.erb
